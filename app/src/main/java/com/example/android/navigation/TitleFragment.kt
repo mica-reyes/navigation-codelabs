8950947cd6d464220d79ab904e23bc7d5c8e9504
mika-reyes <reyessmicael@gmail.com>
2022-10-04 15:11:08 -0300

    task: add a fragment

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,21 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+    private var _binding: FragmentTitleBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= FragmentTitleBinding.inflate(inflater, container,false)
+        return binding?.root
+    }
+}
